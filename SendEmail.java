29b52cdbd85a078d403ca9c45e4d5fea24f3af5c
momskirbyok <nathanringraham@gmail.com>
2017-01-19 09:56:51 -0600

    Cleaned up code.. Added comments
    Too many spaces... had to clean them up eventually.

--- SendEmail.java
+++ SendEmail.java
@@ -1,6 +1,7 @@
 /*
  * SendEmail
  * 1/11/17
+ * Brandon Reid, Nathan Ingraham, Donovan Clofer, Austin Holford
  * Info: The objective of this class is to take in information from the UI to compose an 
  * email to send off to the developer team (us).
  */
@@ -41,90 +42,71 @@ public class SendEmail extends EmailDeveloperUI {
     
     
     public void presentConfirmationBox(String emailAddress, String emailContent) throws MessagingException {
-        
-        
-        
-        
-        
-        
         System.out.println("This method has been called. " + " To: " + emailAddress + " With contents: " + emailContent);
         
-                    Stage confirmStage = new Stage();
+        Stage confirmStage = new Stage();
                     
                     
-                    StackPane confirmPane = new StackPane();
-                    
-                    
-                    
-                    Button confirmEmailButton = new Button("Confirm");
-                    
-                    //confirmEmailButton.setTranslateX(100);
-                    confirmEmailButton.setTranslateY(50);
-                    
-                    confirmEmailButton.setTranslateX(-30);
-                    
-                    Button cancelButton = new Button("Cancel");
-                    
-                    cancelButton.setTranslateX(40);
-                    cancelButton.setTranslateY(50);
-                    
-                    Label confirmItLabel = new Label("Would you like to send the email?");
-                    
-//                    confirmItLabel.setLayoutX(0);
-//                    confirmItLabel.setLayoutY(0);
-                    confirmItLabel.setTranslateX(0);
-                    confirmItLabel.setTranslateY(5);
+        StackPane confirmPane = new StackPane();
                     
                     
                     
-                    ImageView questionMarkImageConfirm = new ImageView("resources/questionmark.png");
+        Button confirmEmailButton = new Button("Confirm");
                     
-                    questionMarkImageConfirm.setLayoutX(0);
-                    questionMarkImageConfirm.setLayoutY(0);
-                    questionMarkImageConfirm.setTranslateY(-30);
+                   
+        confirmEmailButton.setTranslateY(50);
+                    
+        confirmEmailButton.setTranslateX(-30);
+                    
+        Button cancelButton = new Button("Cancel");
+                    
+        cancelButton.setTranslateX(40);
+        cancelButton.setTranslateY(50);
+                    
+        Label confirmItLabel = new Label("Would you like to send the email?");
+                    
+        confirmItLabel.setTranslateX(0);
+        confirmItLabel.setTranslateY(5);
                     
                     
                     
+        ImageView questionMarkImageConfirm = new ImageView("resources/questionmark.png");
                     
-                    confirmPane.getChildren().addAll(confirmEmailButton, confirmItLabel, questionMarkImageConfirm, cancelButton);
-                    
-                    
-                    Scene confirmScene = new Scene(confirmPane, 400,150);
-                    
-                    
-                    confirmStage.initStyle(StageStyle.UNDECORATED); //Prevents user from closing out of the confirmation box
-                    confirmStage.setResizable(false);
-                    confirmStage.setTitle("Confirm Email");
-                    confirmStage.setScene(confirmScene);
-                    confirmStage.show();
-                    
-                    confirmEmailButton.setOnAction(magicAction ->{
+        questionMarkImageConfirm.setLayoutX(0);
+        questionMarkImageConfirm.setLayoutY(0);
+        questionMarkImageConfirm.setTranslateY(-30);  
+        confirmPane.getChildren().addAll(confirmEmailButton, confirmItLabel, questionMarkImageConfirm, cancelButton);
+        Scene confirmScene = new Scene(confirmPane, 400,150);
+        confirmStage.initStyle(StageStyle.UNDECORATED); //Prevents user from closing out of the confirmation box
+        confirmStage.setResizable(false);
+        confirmStage.setTitle("Confirm Email");
+        confirmStage.setScene(confirmScene);
+        confirmStage.show();
+        
+        confirmEmailButton.setOnAction(magicAction ->{
                         
                         
-                    try{
+          try{
                     
-                    PrintWriter emailWriter = new PrintWriter("emailLog.txt");
+               PrintWriter emailWriter = new PrintWriter("emailLog.txt");
                     
-                    sendEmailMessage(emailContent, emailAddress);
+               sendEmailMessage(emailContent, emailAddress);
 
-                    emailWriter.println("\n Email with the contents of \"" +  emailContent + "\" was sent to " + emailAddress);
-                    emailWriter.close();
+               emailWriter.println("\n Email with the contents of \"" +  emailContent + "\" was sent to " + emailAddress);
+               emailWriter.close();
                     
-                    System.out.println("\n Email with the contents of \"" +  emailContent + "\" was sent to " + emailAddress);
-                    
-                    
-                    
+               System.out.println("\n Email with the contents of \"" +  emailContent + "\" was sent to " + emailAddress);
                     } catch(FileNotFoundException e){
                         System.err.println("The log file for email was not found.");
                         System.out.println(e);
                         
                         }
                       catch(MessagingException e){
-                          
+                          System.out.println(e);  
                       }
                     
                         
-                    System.exit(0);
+               System.exit(0);
                     
                     });
                     cancelButton.setOnAction(cancelAction ->{
@@ -134,23 +116,15 @@ public class SendEmail extends EmailDeveloperUI {
                         
                     });
                     
-                    
-                    
-                   
-                    
-                
-                
-            }
+                 }
        
-        
-        
         
     public void sendEmailMessage(String emailContent, String emailAddress) throws MessagingException, AddressException{
         
         
         
-                final String emailAccount = "[removed]@gmail.com";
-                final String password = "[removed]";
+                final String emailAccount = "superuserdevs@gmail.com";
+                final String password = "BPAvirtualteam";
                 Properties mailServerProperties;
                 Session getMailSession;
                 MimeMessage generateMailMessage;
@@ -164,12 +138,17 @@ public class SendEmail extends EmailDeveloperUI {
 		
  
 		
-		// Mail Section 
+		/* MAIL SECTION
+                * When a message is sent, a copy of it will be sent to the bug submitter
+                * The message will be sent to the developer Gmail account &
+                * cc'd to all the developers so that we for sure get the bug report.
+                */
 		getMailSession = Session.getDefaultInstance(mailServerProperties, null);
 		generateMailMessage = new MimeMessage(getMailSession);
 		generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAccount));
-		
-                 generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(emailAddress));
+		generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress("enteremailtocc@gmail.com"));
+             
+                generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(emailAddress));
                  
 		generateMailMessage.setSubject("New Bug Report COPY For SuperUser From: " + emailAddress);
 		String emailBody = "Hello " + emailAddress + "," + " <BR> Thanks for your report. Your report has been sent to the developers of SuperUser. <BR>" + "For your records, this is the report you submitted: <BR><BR>" + emailContent +  "<BR><BR><BR> If you have anything to add to the bug report, feel free to reply to this email." + "<BR><BR> SENT FROM SUPERUSER BUG REPORTER";
